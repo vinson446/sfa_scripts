997744eb90f57ff7a0b014b0c58457c864674b31
Vinson Kok <vinsonkok@yahoo.com>
2020-09-21 00:02:07 -0500

    Implemented _init_from_path using regex

--- src/scenefile.py
+++ src/scenefile.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import re
 
 
 class SceneFile(object):
@@ -24,13 +25,15 @@ class SceneFile(object):
         return self.folder_path / self.filename
 
     def _init_from_path(self, path):
+        descriptor, self.task, ver = re.split(r"\_", path)
+        self.descriptor = re.sub(r"[^a-z0-9]", "", descriptor)
+        ver, ext = re.split(r"\.", ver)
+        self.ver = int(ver.split("v")[-1])
+        self.ext = "." + ext
         path = Path(path)
         self.folder_path = path.parent
-        self.ext = path.suffix
-        self.descriptor, self.task, ver = path.stem.split("_")
-        self.ver = int(ver.split("v")[-1])
 
 
-scene_file = SceneFile("D:/tank_model_v001.ma")
+scene_file = SceneFile("D://tank_model_v001.ma")
 print(scene_file.path)
 print(scene_file.filename)
