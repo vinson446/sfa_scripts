890946b8a05a6dbd3b34039d92b3a4a738867eaf
Vinson Kok <vinsonkok@yahoo.com>
2020-09-20 20:39:04 -0500

    Scenefile constructor

---
+++ src/scenefile.py
@@ -0,0 +1,11 @@
+class SceneFile(object):
+    """An abstract representation of a Scene file."""
+    def __init__(self, folder_path, descriptor, task, ver, ext):
+        self.folder_path = folder_path
+        self.descriptor = descriptor
+        self.task = task
+        self.ver = ver
+        self.ext = ext
+
+scene_file = SceneFile("D:\\", "tank", "model", "v001", ".ma")
+print(scene_file.descriptor)
