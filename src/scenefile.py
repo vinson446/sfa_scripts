953e5d4c86556b01d779eb2986e3460efc464bf3
Vinson Kok <vinsonkok@yahoo.com>
2020-09-20 23:01:14 -0500

    Implemented _init_from_path

--- src/scenefile.py
+++ src/scenefile.py
@@ -3,12 +3,13 @@ from pathlib import Path
 
 class SceneFile(object):
     """An abstract representation of a Scene file."""
-    def __init__(self, folder_path, descriptor, task, ver, ext):
-        self.folder_path = Path(folder_path)
-        self.descriptor = descriptor
-        self.task = task
-        self.ver = ver
-        self.ext = ext
+    def __init__(self, path):
+        self.folder_path = Path()
+        self.descriptor = 'main'
+        self.task = None
+        self.ver = 1
+        self.ext = '.ma'
+        self._init_from_path(path)
 
     @property
     def filename(self):
@@ -22,6 +23,14 @@ class SceneFile(object):
     def path(self):
         return self.folder_path / self.filename
 
+    def _init_from_path(self, path):
+        path = Path(path)
+        self.folder_path = path.parent
+        self.ext = path.suffix
+        self.descriptor, self.task, ver = path.stem.split("_")
+        self.ver = int(ver.split("v")[-1])
 
-scene_file = SceneFile("D:/", "tank", "model", 1, ".ma")
+
+scene_file = SceneFile("D:/tank_model_v001.ma")
 print(scene_file.path)
+print(scene_file.filename)
