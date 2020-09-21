15991aac0138a85b6de36e069ff65bca19773a70
Vinson Kok <vinsonkok@yahoo.com>
2020-09-21 00:10:39 -0500

    Printing properties of SceneFile object

--- src/scenefile.py
+++ src/scenefile.py
@@ -26,7 +26,7 @@ class SceneFile(object):
 
     def _init_from_path(self, path):
         descriptor, self.task, ver = re.split(r"\_", path)
-        self.descriptor = re.sub(r"[^a-z0-9]", "", descriptor)
+        self.descriptor = descriptor[descriptor.rfind("/") + 1:]
         ver, ext = re.split(r"\.", ver)
         self.ver = int(ver.split("v")[-1])
         self.ext = "." + ext
@@ -34,6 +34,11 @@ class SceneFile(object):
         self.folder_path = path.parent
 
 
-scene_file = SceneFile("D://tank_model_v001.ma")
-print(scene_file.path)
+scene_file = SceneFile("D:/sandbox/tank_model_v001.ma")
+print(scene_file.folder_path)
+print(scene_file.descriptor)
+print(scene_file.task)
+print(scene_file.ver)
+print(scene_file.ext)
 print(scene_file.filename)
+print(scene_file.path)
