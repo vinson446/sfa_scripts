6c27dae7cdcf27057d55ac044dfbb8210ce2bfd8
Vinson Kok <vinsonkok@yahoo.com>
2020-10-12 03:53:33 -0500

    Smart Save Tutorial 402 - Create a File Dialogue

--- src/smartsave.py
+++ src/smartsave.py
@@ -29,6 +29,7 @@ class SmartSaveUI(QtWidgets.QDialog):
                             QtCore.Qt.WindowContextHelpButtonHint)
         self.scenefile = SceneFile()
         self.create_ui()
+        self.create_connections()
 
     def create_ui(self):
         self.title_lbl = QtWidgets.QLabel("Smart Save")
@@ -43,6 +44,18 @@ class SmartSaveUI(QtWidgets.QDialog):
         self.main_lay.addStretch()
         self.main_lay.addLayout(self.button_lay)
         self.setLayout(self.main_lay)
+
+    def create_connections(self):
+        self.folder_browse_btn.clicked.connect(self._browse_folder)
+
+    @QtCore.Slot()
+    def _browse_folder(self):
+        """Opens a dialogue box to browse the folder"""
+        folder = QtWidgets.QFileDialog.getExistingDirectory(
+            parent=self, caption="Select folder", dir=self.folder_le.text(),
+            options=QtWidgets.QFileDialog.ShowDirsOnly |
+                    QtWidgets.QFileDialog.DontResolveSymlinks)
+        self.folder_le.setText(folder)
 
     def _create_button_ui(self):
         self.save_btn = QtWidgets.QPushButton("Save")
