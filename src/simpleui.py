4265349c27ed9f9713a3433e2fbd92c0f2c6b06f
Vinson Kok <vinsonkok@yahoo.com>
2020-10-12 00:36:27 -0500

    Parent Window in Maya

--- src/simpleui.py
+++ src/simpleui.py
@@ -1,4 +1,12 @@
+import maya.OpenMayaUI as omui
 from PySide2 import QtWidgets
+from shiboken2 import wrapInstance
+
+
+def maya_main_window():
+    """Return the maya main window widget"""
+    main_window = omui.MQtUtil.mainWindow()
+    return wrapInstance(long(main_window), QtWidgets.QWidget)
 
 
 class SimpleUI(QtWidgets.QDialog):
@@ -8,5 +16,5 @@ class SimpleUI(QtWidgets.QDialog):
         """Constructor"""
         # passing the object SimpleUI as an argument to super()
         # makes this line python 2 and 3 compatible
-        super(SimpleUI, self).__init__();
+        super(SimpleUI, self).__init__(parent=maya_main_window());
         self.setWindowTitle("A Simple UI")
