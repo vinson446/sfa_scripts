f343cf4fe682add33322a2e346ec3b911f7d5772
Vinson Kok <vinsonkok@yahoo.com>
2020-10-12 04:00:38 -0500

    Smart Save Tutorial 403 - Save Button

--- src/smartsave.py
+++ src/smartsave.py
@@ -46,7 +46,19 @@ class SmartSaveUI(QtWidgets.QDialog):
         self.setLayout(self.main_lay)
 
     def create_connections(self):
+        """Connect Signals and Slots"""
         self.folder_browse_btn.clicked.connect(self._browse_folder)
+        self.save_btn.clicked.connect(self._save)
+
+    @QtCore.Slot()
+    def _save(self):
+        """Save the scene"""
+        self.scenefile.folder_path = self.folder_le.text()
+        self.scenefile.descriptor = self.descriptor_le.text()
+        self.scenefile.task = self.task_le.text()
+        self.scenefile.ver = self.ver_sbx.value()
+        self.scenefile.ext = self.ext_lbl.text()
+        self.scenefile.save()
 
     @QtCore.Slot()
     def _browse_folder(self):
@@ -111,7 +123,7 @@ class SmartSaveUI(QtWidgets.QDialog):
 class SceneFile(object):
     """An abstract representation of a Scene file."""
     def __init__(self, path=None):
-        self.folder_path = Path(cmds.workspace(query=True,
+        self._folder_path = Path(cmds.workspace(query=True,
                                                rootDirectory=True)) / "scenes"
         self.descriptor = 'main'
         self.task = 'model'
@@ -124,6 +136,14 @@ class SceneFile(object):
             log.info("Initialize with default properties.")
             return
         self._init_from_path(path)
+
+    @property
+    def folder_path(self):
+        return self._folder_path
+
+    @folder_path.setter
+    def folder_path(self, val):
+        self._folder_path = Path(val)
 
     @property
     def filename(self):
